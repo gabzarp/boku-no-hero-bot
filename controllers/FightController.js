77caae6c4b85a3de27cecc063825593d36e06b67
Gabriel Zarpelon <zarpelon091098@gmail.com>
2020-08-29 23:53:11 -0300

    fight system peeca

--- controllers/FightController.js
+++ controllers/FightController.js
@@ -1,19 +1,20 @@
 const User = require("../models/user")
 const { Message } = require("discord.js")
 
-const status = [
-    'defence',
-    'attack',
-    'power',
-    'defence_power', 
-    'agility', 
-    'precision', 
-    'utility'
-]
+const statusRolls = {
+    'attack': 'defence',
+    'power': 'defence_power',
+    'agility': 'precision'
+}
 
 module.exports = {
     duel: async (params)=>{
         var message = await params.message.channel.send(`<@!${params.user.userId}> is challenging ${params.args[1]}`)
+        if (typeof params.args[1] === 'undefined') {
+            message.channel.send(`You didn't challange anyone. Correct sintax: %duel '@username'.`);
+            return;
+        }
+        
         var enemy = await User.findOne({userId : params.args[1].replace("<@!", "").replace(">", "")})
         if (!enemy) {
             message.channel.send(`Challenger doesn't have a quirk.`);
@@ -25,13 +26,12 @@ module.exports = {
         await message.awaitReactions((reaction, user) => user.id == enemy.userId && (reaction.emoji.name == '👍' || reaction.emoji.name == '👎'),
         { max: 1, time: 30000 }).then(async collected => {
                 if (collected.first().emoji.name == '👍') {
-                    var points = await status.reduce(async (points, status)=>{
-                        if(params.user[status] >= enemy[status]){
-                            points  = await points + 1
-                        }
+                    var points = await Object.keys(statusRolls).reduce(async (points, status)=>{
+                        points = await points + (Math.random() * (params.user[status] - enemy[statusRolls[status]]))
+                        points = await points + (Math.random() * (enemy[statusRolls[status]] - params.user[status]))
                         return points
                     }, Promise.resolve(0))
-                    var winner =  points > 4 ? params.user.name : enemy.name
+                    var winner =  points > 0 ? params.user.name : enemy.name
                     
                     enemy.name.stamina += -1
                     params.user.name.stamina += -1
